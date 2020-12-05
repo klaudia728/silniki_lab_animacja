e5cb8df86b4dcd1276790397feb8495dc9bc65e2
klaudia728 <klaudia728@poczta.fm>
2020-12-05 17:58:01 +0100

    walk and drunk animation, no script

---
+++ Assets/script.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        void Update()
+        {
+            Rigidbody rigidbody = transform.GetComponent<Rigidbody>();
+
+            Vector3 Direction = Vector3.zero;
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                Direction = -Vector3.left;
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                Direction = Vector3.left;
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                Direction = -Vector3.forward;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                Direction = Vector3.forward;
+            }
+
+            rigidbody.AddTorque(Direction * 20f);
+        }
+    }
+
+    
+}
